1ea36fc88570802332f22f5081e1820ba75ca795
ardiaanM <113528715+ardiaanM@users.noreply.github.com>
2022-10-09 14:37:12 +0200

    strings.xml - La till @strings på olika knappar och siffror
    MainActivity.java - La in EditText för number input, float för värdena och boolean för olika metoder för att få ett sluttal.
    
    La till setOnClickListeners för varje knapp så att man får fram ett värde när man klickar på knapparna.
    
    Att göra: Fixa sudda ut knappen, och lägga till något som gör att vi kan få fram, Roten ur, Pythagoras sats, Cirkelns Area och Cylinderns Volym. Kanske ändra designen och knapparna lite, vill också se om det går att ha kvar värdet på det tal du skriver in så att det inte suddas ut när du klickar "+-*/" så du ser vilket tal du har skrivit in istället för att det försvinner.

--- app/src/main/java/com/kyhskola/kalkuppg/MainActivity.java
+++ app/src/main/java/com/kyhskola/kalkuppg/MainActivity.java
@@ -11,37 +11,199 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bt0, bt1, bt2, bt3, bt4, bt5, bt6,
+            bt7, bt8, bt9, btPlus, btSub, btDiv,
+            btMulti, btC, btEqu;
+
+    EditText numberInput;
+
+    float mValueOne, mValueTwo;
+
+    boolean Addition, Subtract, Multiplication, Division;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void Click(View view){
-        Button bt0 =(Button)findViewById(R.id.buttonZero);
-        Button bt1 =(Button)findViewById(R.id.button1);
-        Button bt2 =(Button)findViewById(R.id.button2);
-        Button bt3 =(Button)findViewById(R.id.button3);
-        Button bt4 =(Button)findViewById(R.id.button4);
-        Button bt5 =(Button)findViewById(R.id.button5);
-        Button bt6 =(Button)findViewById(R.id.button6);
-        Button bt7 =(Button)findViewById(R.id.button7);
-        Button bt8 =(Button)findViewById(R.id.button8);
-        Button bt9 =(Button)findViewById(R.id.button9);
-        Button bt00 =(Button)findViewById(R.id.button00);
-        Button btPlus =(Button)findViewById(R.id.buttonAddition);
-        Button btSub =(Button)findViewById(R.id.buttonSubtraction);
-        Button btMulti =(Button)findViewById(R.id.buttonMultiplication);
-        Button btDiv =(Button)findViewById(R.id.buttonDivision);
-        Button btEqu =(Button)findViewById(R.id.buttonEquals);
-        Button btPercent =(Button)findViewById(R.id.buttonPercent);
-        Button btC =(Button)findViewById(R.id.buttonC);
-        Button btDot =(Button)findViewById(R.id.buttonDot);
-        Button btBack =(Button)findViewById(R.id.buttonBack);
+    public void Click(View view) {
+        Button bt0 = (Button) findViewById(R.id.buttonZero);
+        Button bt1 = (Button) findViewById(R.id.button1);
+        Button bt2 = (Button) findViewById(R.id.button2);
+        Button bt3 = (Button) findViewById(R.id.button3);
+        Button bt4 = (Button) findViewById(R.id.button4);
+        Button bt5 = (Button) findViewById(R.id.button5);
+        Button bt6 = (Button) findViewById(R.id.button6);
+        Button bt7 = (Button) findViewById(R.id.button7);
+        Button bt8 = (Button) findViewById(R.id.button8);
+        Button bt9 = (Button) findViewById(R.id.button9);
+        Button bt00 = (Button) findViewById(R.id.button00);
+        Button btPlus = (Button) findViewById(R.id.buttonAddition);
+        Button btSub = (Button) findViewById(R.id.buttonSubtraction);
+        Button btMulti = (Button) findViewById(R.id.buttonMultiplication);
+        Button btDiv = (Button) findViewById(R.id.buttonDivision);
+        Button btEqu = (Button) findViewById(R.id.buttonEquals);
+        Button btPercent = (Button) findViewById(R.id.buttonPercent);
+        Button btC = (Button) findViewById(R.id.buttonC);
+        Button btDot = (Button) findViewById(R.id.buttonDot);
+        Button btBack = (Button) findViewById(R.id.buttonBack);
+        numberInput = (EditText) findViewById(R.id.numberInput);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
 
 
-        int b0 = Integer.parseInt(bt0.getText().toString());
+            public void onClick(View v) {
+
+            }
+
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "2");
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "3");
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "4");
+            }
+        });
+        bt5.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "5");
+            }
+        });
+        bt6.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "6");
+            }
+        });
+        bt7.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "7");
+            }
+
+        });
+        bt8.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "8");
+            }
+        });
+        bt9.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "9");
+            }
+        });
+        bt0.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "0");
+            }
+        });
+        btDot.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + ".");
+            }
+        });
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "1");
+            }
+        });
+        bt00.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText(numberInput.getText() + "00");
+            }
+        });
+
+
+        btPlus.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (numberInput == null) {
+                    numberInput.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(numberInput.getText() + "");
+                    Addition = true;
+                    numberInput.setText(null);
+                }
+            }
+        });
+        btSub.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(numberInput.getText() + "");
+                Subtract = true;
+                numberInput.setText(null);
+            }
+        });
+        btMulti.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(numberInput.getText() + "");
+                Multiplication = true;
+                numberInput.setText(null);
+            }
+        });
+        btDiv.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(numberInput.getText() + "");
+                Division = true;
+                numberInput.setText(null);
+            }
+        });
+
+
+        btEqu.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(numberInput.getText() + "");
+                if (Addition == true) {
+                    numberInput.setText(mValueOne + mValueTwo + "");
+                    Addition = false;
+                }
+                if (Subtract == true) {
+                    numberInput.setText(mValueOne - mValueTwo + "");
+                    Subtract = false;
+                }
+                if (Multiplication == true) {
+                    numberInput.setText(mValueOne * mValueTwo + "");
+                    Multiplication = false;
+                }
+                if (Division == true) {
+                    numberInput.setText(mValueOne / mValueTwo + "");
+                    Division = false;
+                }
+            }
+        });
+        btC.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                numberInput.setText("");
+            }
+        });
+
+        int num0 = Integer.parseInt(bt0.getText().toString());
     }
+
 
 
 }
